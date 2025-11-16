e10449e0c8336f6e19f3965df2b4618f7f27dfa3
jiyeon <jiyeon.back.dev@gmail.com>
2025-11-16 20:08:19 +0900

    Initial commit

---
+++ src/main/java/org/jiyeon/sns/SnsApplication.java
@@ -0,0 +1,13 @@
+package org.jiyeon.sns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnsApplication.class, args);
+    }
+
+}
